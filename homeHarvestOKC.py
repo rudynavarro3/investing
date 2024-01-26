dcc3b9ec08a65737350ef9243209208bdd199ea4
Rudy Navarro <rudy.navarro3@gmail.com>
2024-01-26 08:18:23 -0600

    adding option to update historical data

--- homeHarvestOKC.py
+++ homeHarvestOKC.py
@@ -5,48 +5,60 @@ import pandas as pd
 # Input Variables
 radius = 100
 # listing_type = "sold" # (sold, for_sale, for_rent, pending)
-past_days = 360
-# date_from="2023-05-01" # alternative to past_days 
-# date_to="2023-05-28"
+# past_days = 720
+# date_from="2022-01-01" # alternative to past_days 
+# date_to=datetime.now().strftime('%Y-%m-%d')
 # mls_only=True  # only fetch MLS listings
 # proxy="http://user:pass@host:port"  # use a proxy to change your IP address
 # scrape_property(radius=radius, location="73003", listing_type=listing_type, past_days=past_days, mls_only=mls_only, proxy=proxy)
 
-locations = ["Oklahoma City, OK", "Seward, OK", "Guthrie, OK", "Edmond, OK"]
-for location in locations:
-    if 'sold' not in locals():
-        sold = scrape_property(radius=radius, location=location, listing_type="sold", past_days=past_days)
-        print(f"Number of sold properties in {location}: {len(sold)}")
-    else:
-        df = scrape_property(radius=radius, location=location, listing_type="sold", past_days=past_days)
-        print(f"Number of sold properties in {location}: {len(df)}")
-        sold = pd.concat([sold,df])
+update_historical = False
+# years = ['2020','2021','2022','2023','2024']
+years = ['2024']
+for idx, year in enumerate(years):
+    date_from=f"{year}-01-01" # alternative to past_days 
+    date_to=f"{year}-12-31"
+    past_days = 720
+    locations = ["Oklahoma City, OK", "Seward, OK", "Guthrie, OK", "Edmond, OK", "Norman, OK", "Moore, OK"]
+    for location in locations:
+        if update_historical:
+            if 'sold' not in locals():
+                sold = scrape_property(radius=radius, location=location, listing_type="sold", date_from=date_from, date_to=date_to)
+                print(f"Number of sold properties in {location}: {len(sold)}")
+            else:
+                df = scrape_property(radius=radius, location=location, listing_type="sold",date_from=date_from, date_to=date_to)
+                print(f"Number of sold properties in {location}: {len(df)}")
+                sold = pd.concat([sold,df])
+        if year == years[-1]:
+            if 'selling' not in locals():
+                selling = scrape_property(radius=radius, location=location, listing_type="for_sale", date_from=date_from, date_to=date_to)
+                print(f"Number of selling properties in {location}: {len(selling)}")
+            else:
+                df = scrape_property(radius=radius, location=location, listing_type="for_sale", date_from=date_from, date_to=date_to)
+                print(f"Number of selling properties in {location}: {len(df)}")
+                selling = pd.concat([selling,df])
 
-    if 'selling' not in locals():
-        selling = scrape_property(radius=radius, location=location, listing_type="for_sale", past_days=past_days)
-        print(f"Number of selling properties in {location}: {len(selling)}")
-    else:
-        df = scrape_property(radius=radius, location=location, listing_type="for_sale", past_days=past_days)
-        print(f"Number of selling properties in {location}: {len(df)}")
-        selling = pd.concat([selling,df])
+            if 'renting' not in locals():
+                renting = scrape_property(radius=radius, location=location, listing_type="for_rent", date_from=date_from, date_to=date_to)
+                print(f"Number of renting properties in {location}: {len(renting)}")
+            else:
+                df = scrape_property(radius=radius, location=location, listing_type="for_rent", date_from=date_from, date_to=date_to)
+                print(f"Number of renting properties in {location}: {len(df)}")
+                renting = pd.concat([renting,df])
 
-    if 'renting' not in locals():
-        renting = scrape_property(radius=radius, location=location, listing_type="for_rent", past_days=past_days)
-        print(f"Number of renting properties in {location}: {len(renting)}")
-    else:
-        df = scrape_property(radius=radius, location=location, listing_type="for_rent", past_days=past_days)
-        print(f"Number of renting properties in {location}: {len(df)}")
-        renting = pd.concat([renting,df])
+            if 'pending' not in locals():
+                pending = scrape_property(radius=radius, location=location, listing_type="pending", date_from=date_from, date_to=date_to)
+                print(f"Number of pending properties in {location}: {len(pending)}")
+            else:
+                df = scrape_property(radius=radius, location=location, listing_type="pending", date_from=date_from, date_to=date_to)
+                print(f"Number of pending properties in {location}: {len(df)}")
+                pending = pd.concat([pending,df])
 
-    if 'pending' not in locals():
-        pending = scrape_property(radius=radius, location=location, listing_type="pending", past_days=past_days)
-        print(f"Number of pending properties in {location}: {len(pending)}")
-    else:
-        df = scrape_property(radius=radius, location=location, listing_type="pending", past_days=past_days)
-        print(f"Number of pending properties in {location}: {len(df)}")
-        pending = pd.concat([pending,df])
+    if update_historical:
+        sold.to_csv(f"data/homeHarvest/HomeHarvest_OKC_{year}_sold.csv", index=False)
+        del sold
 
-sold.to_csv(f"data/homeHarvest/HomeHarvest_OKC_sold.csv", index=False)
-selling.to_csv(f"data/homeHarvest/HomeHarvest_OKC_selling.csv", index=False)
-renting.to_csv(f"data/homeHarvest/HomeHarvest_OKC_renting.csv", index=False)
-pending.to_csv(f"data/homeHarvest/HomeHarvest_OKC_pending.csv", index=False)
+    if idx == len(years)-1:
+        selling.to_csv(f"data/homeHarvest/HomeHarvest_OKC_selling.csv", index=False)
+        renting.to_csv(f"data/homeHarvest/HomeHarvest_OKC_renting.csv", index=False)
+        pending.to_csv(f"data/homeHarvest/HomeHarvest_OKC_pending.csv", index=False)
